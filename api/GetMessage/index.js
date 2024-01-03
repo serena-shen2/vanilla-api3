1810b960071f26e1b576848bb1aa508642c3041b
Serena Shen <108768434+serena-shen2@users.noreply.github.com>
2024-01-02 18:11:18 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API046" },
+    body: { text: "Hello from the API0346" },
   };
 };
